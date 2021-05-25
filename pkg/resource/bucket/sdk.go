jaypipes/ack-s3-controller

implement Bucket update code paths
A custom update method now allows an S3 Bucket to have its ACL
attributes and object lock enablement attribute to be updated after the
Bucket has been created.

Issue aws/aws-controllers-k8s#653

pkg/resource/bucket/sdk.go
@@ -175,8 +175,7 @@ func (rm *resourceManager) sdkUpdate(
 	latest *resource,
 	diffReporter *ackcompare.Reporter,
 ) (*resource, error) {
-	// TODO(jaypipes): Figure this out...
-	return nil, ackerr.NotImplemented
+	return rm.customUpdateBucket(ctx, desired, latest, diffReporter)
 }
 
 // sdkDelete deletes the supplied resource in the backend AWS service API

aws-controllers-k8s/s3-controller

Co-authored-by: Amine Hilaly <hilalyamine@gmail.com>

pkg/resource/bucket/sdk.go
@@ -180,8 +180,7 @@ func (rm *resourceManager) sdkUpdate(
 	latest *resource,
 	delta *ackcompare.Delta,
 ) (*resource, error) {
-	// TODO(jaypipes): Figure this out...
-	return nil, ackerr.NotImplemented
+	return rm.customUpdateBucket(ctx, desired, latest, delta)
 }
 
 // sdkDelete deletes the supplied resource in the backend AWS service API